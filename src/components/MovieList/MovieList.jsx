e2a8b4285fe367bade4d4c39f1f494317683f9fe
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-07-19 14:34:31 +0300

    sol

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,14 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard';
+import moviesFromServer from '../../api/movies.json';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = () => {
+  return (
+    <div className="movies">
+      {moviesFromServer.map(movie => (
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        <MovieCard key={movie.imdbId} {...movie} />
+      ))}
+    </div>
+  );
+};
